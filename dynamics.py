c6c249a78b418c5009ce10c3704c5cfbfcbe7086
OsQarM <oscar.michel@qilimanjaro.tech>
2025-09-16 14:35:54 +0200

    Started refactoring the 2-step protocol

--- dynamics.py
+++ dynamics.py
@@ -1,5 +1,72 @@
 import qutip as qt
 import numpy as np
+import Hamiltonian as Ham
+import model_building as md
+import data_handling as dh
+
+
+def TwoStepAlgorithm(N, lmd, J, initial_state_dict, ti, tf, Nstep):
+    """
+    
+    Runs the 2-step protocol to achieve quantum transport with domain walls
+
+    Args:
+        N: Integer size of system (chain length in qubits)
+        lmd: Float prefactor of the Hamiltonian
+        J: Float domain wall coupling
+        initial_state_dict: Dictionary containing states and weights
+        ti: Float initial simulation time
+        tf: Float final simulation time
+        Nsteps: Integer number of timesteps
+
+    Returns:
+
+    """
+
+    # Create DW registers and whole systems for initial and target state
+    initial_state = md.crate_domain_wall_state(initial_state_dict, register='Alice')
+    final_state   = md.crate_domain_wall_state(initial_state_dict, register='Bob')
+
+    initial_chain = md.initialize_general_system(N, initial_state, register='Alice')
+    final_chain   = md.initialize_general_system(N, final_state, register='Bob')
+
+    #Create Hamiltonians for the 2 separate steps
+    register_size = len(initial_state.dims[0])
+
+    H_transport = Ham.Hamiltonian(system_size = N,
+                         mode = "forward",
+                         lambda_factor = lmd,
+                         global_J = J
+                         )
+
+    H_reset     = Ham.Hamiltonian(system_size = N,
+                         mode = "backward",
+                         lambda_factor = lmd,
+                         register_size = register_size,
+                         global_J = J
+                         )
+    
+
+    result_transport         = time_evolution                (H_transport, initial_chain, ti, tf, Nstep)
+    full_fidelity_transport  = calculate_full_fidelity       (result_transport, final_chain)
+    magnetizations_transport = calculate_z_expectation_values(result_transport, H_transport.sz_list)
+
+    middle_chain = result_transport.states[-1]
+    result_reset         = time_evolution                (H_reset, middle_chain, ti, tf*1.12, int(Nstep*1.12)) #Additional time for validation and aesthetics
+    full_fidelity_reset  = calculate_full_fidelity       (result_reset, final_chain)
+    magnetizations_reset = calculate_z_expectation_values(result_reset, H_reset.sz_list)
+
+    total_full_fidelity = np.concatenate((full_fidelity_transport, full_fidelity_reset), axis=0)
+    magnetizations      = np.concatenate((magnetizations_transport, magnetizations_reset), axis=0)
+
+    # data storage test
+    f_filename = dh.create_data_filename(N, J, lmd, initial_state_dict, base_name="fidelity", extension = "npy")
+    z_filename = dh.create_data_filename(N, J, lmd, initial_state_dict, base_name="z_expectation", extension = "npy")
+
+    dh.save_numpy_array(total_full_fidelity, f'data_files/fidelity/{f_filename}')
+    dh.save_numpy_array(magnetizations, f'data_files/z_expectation/{z_filename}')
+
+    return total_full_fidelity, magnetizations
 
 def time_evolution(Ham, initial_state, initial_time, final_time, timesteps):
     '''
